d6efb72669aa1fc662c2cdfa394c7bee685334d9
Om Raj <34886077+Om-Raj@users.noreply.github.com>
2023-04-09 03:58:33 +0530

    Optimized algorithm

--- gcd.rs
+++ gcd.rs
@@ -11,58 +11,34 @@
 * @return u64 - the GCD of the two numbers a and b
 */
 
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    // If one of the numbers is zero, return the other number as the GCD
+    if a == 0 { return b; }
+    if b == 0 { return a; }
 
-fn gcd(mut a: u64, mut b: u64) -> u64
-{
-    // If one of the inputs is zero, return the other input as the GCD
-    if a == 0 {
-        return b;
-    }
-    if b == 0 {
-        return a;
-    }
-    
-    // Initialize a factor to keep track of the common factors of 2
-    let mut f : u64 = 1;
+    // Compute the highest power of 2 that divides both a and b
+    let shift = (a | b).trailing_zeros();
 
-    // Continue looping until we find the GCD
+    // Divide a and b by this power of 2 and further divide to remove remaining factors of 2 in a
+    a >>= a.trailing_zeros();
+    b >>= shift;
+
+    // Use the Euclidean algorithm to compute the GCD
     loop {
-        // If a and b are equal, return the GCD multiplied by the factor
-        if a == b {
-            return a * f;
+        b >>= b.trailing_zeros();
+        if a > b {
+            let temp = a;
+            a = b;
+            b = temp;
         }
-
-        // If a is even and b is even, divide both by 2 and multiply the factor by 2
-        if a & 1 == 0 {
-            if b & 1 == 0 {
-                a = a >> 1;
-                b = b >> 1;
-                f = f << 1;
-                continue;
-            }
-            // If only a is even, divide a by 2
-            a = a >> 1;
-            continue;
-        }
-
-        // If a is odd and b is even, divide b by 2
-        else {
-            if b & 1 == 1 {
-                // If a is greater than b, subtract b from a
-                if a > b {
-                    a = a - b;
-                    continue;
-                }
-                // If b is greater than a, subtract a from b
-                b = b - a;
-                continue;
-            }
-            // If only b is even, divide b by 2
-            b = b >> 1;
-            continue;
-        }
+        b -= a;
+        if b == 0 { break; }
     }
+
+    // Multiply the GCD by the power of 2 that we divided out
+    a << shift
 }
+
 
 // Creating a main() to call the gcd() function with demo values
 fn main() {
